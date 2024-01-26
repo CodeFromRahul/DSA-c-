18a2e9708fdf7c203c991a9b4610bb1b76cb7979
Rahul <rahulvish194002@gmail.com>
2024-01-26 13:12:27 +0530

    Finding Pivot index of an array

---
+++ pivot.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int pivotIndex(int arr[],int n){
+    int s =0;
+    int e = n-1;
+    int mid = s+(e-s)/2;
+    while (s<e)
+    {
+       if(arr[mid]>=arr[0]){
+        s=mid+1;
+       }
+       else{
+        e=mid;
+       }
+       mid =s+(e-s)/2;
+    }
+    return s ;
+}
+int main(){
+    
+    int arr[5]={3,4,5,13,1};
+    int pivot = pivotIndex(arr,5);
+    cout<<"The pivot index is "<<pivot<<endl;
+    return 0;
+}
