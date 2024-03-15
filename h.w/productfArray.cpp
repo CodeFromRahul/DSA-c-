e87303611f5709077d653281050584ab61f3a14a
Rahul <rahulvish194002@gmail.com>
2024-03-15 21:59:16 +0530

    Function for Product of Array Except Self

---
+++ h.w/productfArray.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, 0);
+        int product = 1;
+        int zeros = 0;
+        
+        for (int num : nums) {
+            if (num == 0) {
+                zeros++;
+                continue;
+            }
+            product *= num;
+        }
+
+        if (zeros == 1) {
+            for (int i = 0; i < n; i++) {
+                ans[i] = nums[i] == 0 ? product : 0;
+            }
+        } else if (zeros == 0) {
+            for (int i = 0; i < n; i++) {
+                ans[i] = product / nums[i];
+            }
+        }
+
+        return ans;
+    }
+};
